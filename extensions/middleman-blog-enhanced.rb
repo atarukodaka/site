8bea19c72876a78ff6f88bc7abae51eaa95c118a
Ataru Kodaka <ataru.kodaka@gmail.com>
2014-11-29 00:11:46 +0900

    add recent_articles_date_hash

--- extensions/middleman-blog-enhanced.rb
+++ extensions/middleman-blog-enhanced.rb
@@ -4,6 +4,16 @@ module Middleman
       def categories
         articles.group_by {|p| p.data.category }
       end
+
+      def recent_articles_date_hash(num_display = 10)
+        hash = {}
+        articles.first(num_display).each do |article|
+          dt = article.date.to_date
+          hash[dt] ||= []
+          hash[dt] << article
+        end
+        return hash.sort {|(dt1, v1), (dt2, v2)| dt2 <=> dt1 } # reverse sorted by date
+      end
     end
 
     class Extension < Middleman::Extension
--- helpers/site_helpers.rb
+++ helpers/site_helpers.rb
@@ -13,28 +13,6 @@ module SiteHelpers
   def select_resources_by(key, value)
     sitemap.resources.select {|p| p.send(key.to_sym) == value}
   end
-=begin
-  def categories
-    select_html_pages.group_by {|p| p.data.category }   # .reject {|category, pages| category.to_s == "" || pages.count == 0 }  # .each {|category, pages|
-  end
- 
-  def tags
-    hash = {}
-    select_html_pages.each do |page|
-      next if page.data.tag.to_s == ""
-
-      page.tags.each do |t|
-        hash[t] ||= [] 
-        hash[t] << page
-      end
-    end
-    hash
-  end
-=end
-
- def series
-    select_html_pages.reject {|p| p.data.series.nil?}.group_by {|p| p.data.series }
-  end
 
   ################
   def recent_pages(num_display = 10)
